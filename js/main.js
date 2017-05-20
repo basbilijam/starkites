6b3cb7b952abb7f1fcfbb7cd8379ab03bed39897
basbilijam <basbilijam@gmail.com>
2017-05-20 11:46:10 +0200

    color hamburger menu, added slow scroll to anchors

--- js/main.js
+++ js/main.js
@@ -31,6 +31,14 @@ $(document).ready(function(){
    $(".nav").find(".active").removeClass("active");
    $(this).parent().addClass("active");
  });
+
+   ////// slow scroll to anchors on main page //
+  $(document).on('click', 'a', function(event){
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+  });
 });
 
 
